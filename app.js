814127eb17b0d6dcd8bdf6cfc0f26b3c0f2c3e6a
AlexOneUp <ruanalex11235@gmail.com>
2021-07-27 16:11:17 -0400

    CRUD: DELETE and PUT additions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const methodOverride = require('method-override');
+
 const Campground = require('./models/campgrounds');
 
 mongoose.connect('mongodb://localhost:27017/campgrounds', {
@@ -22,6 +24,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Parse form body views/campgrounds/new.ejs
 app.use(express.urlencoded({ extended: true }));
+// for PUT, PATCH, DELETE routes
+app.use(methodOverride('_method'));
 
 // Landing page
 app.get('/', (req, res) => {
@@ -42,6 +46,7 @@ app.get('/campgrounds', async (req, res) => {
 app.get('/campgrounds/new', async (req, res) => {
     res.render('campgrounds/new');
 });
+
 // POST new Campground from new.ejs form data onto database : campgrounds
 app.post('/campgrounds', async (req, res) => {
     const newCampground = new Campground(req.body.campground);
@@ -55,6 +60,29 @@ app.get('/campgrounds/:id', async (req, res) => {
     // Find for all Campground models in our DB  
     const campground = await Campground.findById(req.params.id); 
     res.render('campgrounds/show', { campground });
+});
+
+// Serves the form to be able to EDIT campgrounds
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    // Find for all Campground models in our DB  
+    const campground = await Campground.findById(req.params.id); 
+    res.render('campgrounds/edit', { campground });
+});
+
+// Update using methodOverride to update campgrounds by ID
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+
+});
+
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+
 });
 
 

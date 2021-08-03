73b7fc54205a4070009a743e956fa0a850bb150c
AlexOneUp <ruanalex11235@gmail.com>
2021-08-03 00:45:13 -0400

    Edited more Validation

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
+
+const {campgroundSchema} = require('./validationSchemas');
+
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 
@@ -31,6 +33,18 @@ app.use(express.urlencoded({ extended: true }));
 // for PUT, PATCH, DELETE routes
 app.use(methodOverride('_method'));
 
+// Validate Middleware
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(element => element.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+
+}
+
 // Landing page
 app.get('/', (req, res) => {
     res.render('home')
@@ -51,25 +65,11 @@ app.get('/campgrounds/new', catchAsync(async (req, res) => {
     res.render('campgrounds/new');
 }));
 
-// POST new Campground from new.ejs form data onto database : campgrounds
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+// Validate Middleware
 
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required(),
-            
-        }).required()
-    })
-    const {error} = campgroundSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(element => element.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    console.log(result);
+// POST new Campground from new.ejs form data onto database : campgrounds
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+
     const newCampground = new Campground(req.body.campground);
     await newCampground.save();
     res.redirect(`/campgrounds/${newCampground._id}`);
@@ -92,7 +92,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 }));
 
 // Update using methodOverride to update campgrounds by ID
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);

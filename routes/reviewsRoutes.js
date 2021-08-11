64153d0dd03c0440db2457a667629f7e512abe8a
AlexOneUp <ruanalex11235@gmail.com>
2021-08-11 16:15:56 -0400

    Authorization: Restructure middleware inside reviewsRoutes

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -5,10 +5,13 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campgrounds');
 const Review = require('../models/review');
 const { validateReview } = require('../middleware');
+const { isLoggedIn } = require('../middleware');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();

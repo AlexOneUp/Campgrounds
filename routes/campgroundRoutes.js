28c75d30def9b8ee9ffaca2b329918e5b7ff4463
AlexOneUp <ruanalex11235@gmail.com>
2021-08-12 15:13:10 -0400

    Restructuring: restructuring with express : router.route()

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -3,36 +3,62 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 
 const Campground = require('../models/campgrounds');
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const campgroundsController = require('../controllers/campgroundsController');
 // Joi schema
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+// Controller for MVC
+const campgroundsController = require('../controllers/campgroundsController');
 
-
-/**
- * CRUD functionality for campgrounds
+/**__________________________________________________________________________
+ * NEW RESTRUCTURING PATTERN WITH router.route()
+ * Look up express docs for this.
+ * __________________________________________________________________________ 
  */
-// Diplay all camp grounds
-router.get('/', catchAsync(campgroundsController.index));
+router.route('/')
+    .get(catchAsync(campgroundsController.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgroundsController.createCampground))
 
 // GET form to POST new Campground
 router.get('/new', isLoggedIn, campgroundsController.renderNewForm);
 
-// Validate Middleware
-
-// POST new Campground from new.ejs form data onto database : campgrounds
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgroundsController.createCampground))
-
-// Look up corresponding campground in database
-// GET async function
-router.get('/:id', catchAsync(campgroundsController.showCampground));
+router.route('/:id')
+    .get(catchAsync(campgroundsController.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundsController.updateCampground))
+    .delete(isLoggedIn, catchAsync(campgroundsController.deleteCampground))
 
 // Serves the form to be able to EDIT campgrounds
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgroundsController.renderEditForm));
 
-// Update using methodOverride to update campgrounds by ID
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundsController.updateCampground));
-
-
-router.delete('/:id', isLoggedIn, catchAsync(campgroundsController.deleteCampground));
-
 module.exports = router;
+
+
+
+/**__________________________________________________________________________
+ * OLD WAY OF STRUCTURING ROUTES without router.route()
+ * __________________________________________________________________________
+ */
+
+// /**
+//  * CRUD functionality for campgrounds
+//  */
+// // Diplay all camp grounds
+// // router.get('/', catchAsync(campgroundsController.index));
+
+// // GET form to POST new Campground
+// router.get('/new', isLoggedIn, campgroundsController.renderNewForm);
+
+// // Validate Middleware
+// // POST new Campground from new.ejs form data onto database : campgrounds
+// router.post('/', isLoggedIn, validateCampground, catchAsync(campgroundsController.createCampground))
+
+// // Look up corresponding campground in database
+// // GET async function
+// router.get('/:id', catchAsync(campgroundsController.showCampground));
+
+// // Serves the form to be able to EDIT campgrounds
+// router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgroundsController.renderEditForm));
+
+// // Update using methodOverride to update campgrounds by ID
+// router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundsController.updateCampground));
+
+
+// router.delete('/:id', isLoggedIn, catchAsync(campgroundsController.deleteCampground));
--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,34 @@ const User = require('../models/user');
 const passport = require('passport');
 const authController = require('../controllers/authController');
 
-router.get('/register', authController.renderRegister);
-
-router.post('/register', catchAsync(authController.register));
-
-router.get('/login', authController.renderLogin)
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), authController.login);
-
+/**__________________________________________________________________________
+ * NEW RESTRUCTURING PATTERN WITH router.route()
+ * Look up express docs for this.
+ * __________________________________________________________________________
+ */
+router.route('/register')
+    .get(authController.renderRegister)
+    .post(authController.register)
+router.route('/login')
+    .get(authController.renderLogin)
+    .post(authController.login)
 router.get('/logout', authController.logout);
 
 module.exports = router;
+
+
+
+/**__________________________________________________________________________
+ * OLD WAY OF STRUCTURING ROUTES without router.route()
+ * __________________________________________________________________________ 
+ */
+
+// router.get('/register', authController.renderRegister);
+
+// router.post('/register', catchAsync(authController.register));
+
+// router.get('/login', authController.renderLogin)
+
+// router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), authController.login);
+
+// router.get('/logout', authController.logout);

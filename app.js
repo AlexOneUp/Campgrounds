6316ffc6a7d53a819a2cca875bdf0a96e402e9b9
AlexOneUp <ruanalex11235@gmail.com>
2021-08-16 14:36:27 -0400

    First Deployment: Fixing Heroku Errors and making port variables

--- app.js
+++ app.js
@@ -188,9 +188,9 @@ app.use((err, req, res, next) => {
 });
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Serving port 3000")
+    console.log(`Serving port ${port}`)
 });
 
 
